d17d9afd1f2bb17d0d285a675f80512b860b43be
qianyf <f123456>
2018-09-05 18:20:26 +0800

    update version  on splash

--- app/src/main/java/com/coderstory/FTool/activity/SplashActivity.java
+++ app/src/main/java/com/coderstory/FTool/activity/SplashActivity.java
@@ -5,7 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.coderstory.FTool.BuildConfig;
 import com.coderstory.FTool.R;
 
 public class SplashActivity extends Activity {
@@ -19,29 +21,13 @@ public class SplashActivity extends Activity {
         setContentView(R.layout.activity_splash);
         mContext = this;
 
+        ((TextView) findViewById(R.id.version_name)).setText(BuildConfig.VERSION_NAME);
         //倒计时返回主界面
         new AsyncTask<Void, Void, Integer>() {
             @Override
             protected void onPostExecute(Integer result) {
-
-             /*   if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
-                    new Thread() {
-                        @Override
-                        public void run() {
-                            Looper.prepare();
-                            Toast.makeText(mContext, getString(R.string.not_support_android_target_version)+Build.VERSION.SDK_INT, Toast.LENGTH_LONG).show();
-                            Looper.loop();
-                        }
-                    }.start();
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                } else {*/
                 Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
-                /*   }*/
                 finish();
             }
 
