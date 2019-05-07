baf28f45c9f3f5f44502e231fca4fd2e6fba5679
qianyf <f123456>
2019-05-07 18:38:54 +0800

    修复隐藏桌面图标

--- app/src/main/java/com/coderstory/purify/module/HideApp.java
+++ app/src/main/java/com/coderstory/purify/module/HideApp.java
@@ -30,7 +30,18 @@ public class HideApp extends XposedHelper implements IModule {
             if (!value.equals("")) {
                 final List<String> hideAppList = Arrays.asList(value.split(":"));
                 XposedBridge.log("load config" + value);
+
+                // 下面2个hook是一个东西
                 findAndHookMethod("com.meizu.flyme.launcher.cb", loadPackageParam.classLoader, "b", ComponentName.class, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        ComponentName componentName = (ComponentName) param.args[0];
+                        if (hideAppList.contains(componentName.getPackageName())) {
+                            param.setResult(true);
+                        }
+                    }
+                });
+                findAndHookMethod("com.meizu.flyme.launcher.cd", loadPackageParam.classLoader, "a", ComponentName.class, new XC_MethodHook() {
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param) {
                         ComponentName componentName = (ComponentName) param.args[0];
