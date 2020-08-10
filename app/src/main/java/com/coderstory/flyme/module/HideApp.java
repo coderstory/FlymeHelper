9e2b927d88a1539004080d83a9c5526050a6e307
coderstory <coderstory@163.com>
2020-08-10 21:53:19 +0800

    Revert "不稳定的隐藏桌面图标"
    This reverts commit da064d1c

--- app/src/main/java/com/coderstory/flyme/module/HideApp.java
+++ app/src/main/java/com/coderstory/flyme/module/HideApp.java
@@ -13,7 +13,6 @@ import java.util.stream.Collectors;
 
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources;
@@ -28,30 +27,16 @@ public class HideApp extends XposedHelper implements IModule {
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) {
+
+
         if (loadPackageParam.packageName.equals("com.meizu.flyme.launcher")) {
             // bl.add(new ComponentName("com.android.vending", "com.android.vending.MarketWidgetProvider"));
             final String value = prefs.getString("Hide_App_List", "");
             XposedBridge.log("load config" + value);
             if (!value.equals("")) {
+                final List<String> hideAppList = Arrays.asList(value.split(":"));
                 if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                    String value2 = prefs.getString("Hide_App_Name_List", "");
-                    final List<String> hideAppList = Arrays.asList(value2.split(":"));
-                    //  public void addItem(Context context, com.android.launcher3.ItemInfo item, boolean newItem) {
-                    // CharSequence title;
-                    hookAllMethods("com.android.launcher3.model.BgDataModel", loadPackageParam.classLoader, "addItem", new XC_MethodReplacement() {
-                        @Override
-                        protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-                            CharSequence title = (CharSequence) XposedHelpers.getObjectField(param.args[1], "title");
-                            XposedBridge.log("GGG" + (title == null ? "null" : title.toString()));
-                            if (title == null || !hideAppList.contains(title.toString())) {
-                                XposedBridge.invokeOriginalMethod(param.method, param.thisObject, param.args);
-                            }
-                            return null;
-                        }
-                    });
-
                 } else {
-                    final List<String> hideAppList = Arrays.asList(value.split(":"));
                     Class clazz = findClass("com.meizu.flyme.launcher.co", loadPackageParam.classLoader);
                     findAndHookMethod("com.meizu.flyme.launcher.MzWidgetGroupView", loadPackageParam.classLoader, "a", clazz, new XC_MethodHook() {
                         @Override
