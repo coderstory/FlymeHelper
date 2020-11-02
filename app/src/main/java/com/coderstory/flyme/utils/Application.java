3b8cf02e4407bcb4c89d7615f0bd927d269674fd
CoderStory <CoderStory@163.com>
2020-11-02 09:22:30 +0800

    修改失效日期

--- app/src/main/java/com/coderstory/flyme/utils/Application.java
+++ app/src/main/java/com/coderstory/flyme/utils/Application.java
@@ -1,5 +1,7 @@
 package com.coderstory.flyme.utils;
 
+import android.os.Debug;
+
 import com.umeng.commonsdk.UMConfigure;
 
 
@@ -16,7 +18,9 @@ public class Application extends android.app.Application {
         UMConfigure.setLogEnabled(false);
         UMConfigure.init(Application.this, Misc.token, Misc.channel, UMConfigure.DEVICE_TYPE_PHONE, "");
         mInstance = this;
-        System.loadLibrary("Utils");
+        if (!Debug.isDebuggerConnected()) {
+            System.loadLibrary("Utils");
+        }
         CrashHandler crashHandler = CrashHandler.getInstance();
         crashHandler.init(getApplicationContext());
         Cpp.initCpp(mInstance);
