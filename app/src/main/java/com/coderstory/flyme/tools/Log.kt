0cc0c8bead45b8fdcd27f43aa7dc54bfd803bf6d
CoderStory <qianyf2015@gmail.com>
2021-07-21 09:19:03 +0800

    整理代码

---
+++ app/src/main/java/com/coderstory/flyme/tools/Log.kt
@@ -0,0 +1,65 @@
+@file:Suppress("unused")
+
+package com.coderstory.flyme.tools
+
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import de.robv.android.xposed.XposedBridge
+import android.util.Log as ALog
+
+object Log {
+
+    private val handler by lazy { Handler(Looper.getMainLooper()) }
+    private var toast: Toast? = null
+
+
+    @JvmStatic
+    private fun doLog(f: (String, String) -> Int, obj: Any?, toXposed: Boolean = false) {
+        val str = if (obj is Throwable) ALog.getStackTraceString(obj) else obj.toString()
+
+        if (str.length > maxLength) {
+            val chunkCount: Int = str.length / maxLength
+            for (i in 0..chunkCount) {
+                val max: Int = maxLength * (i + 1)
+                if (max >= str.length) {
+                    doLog(f, str.substring(maxLength * i))
+                } else {
+                    doLog(f, str.substring(maxLength * i, max))
+                }
+            }
+        } else {
+            f("FlymeHelper", str)
+            if (toXposed)
+                XposedBridge.log("FlymeHelper : $str")
+        }
+    }
+
+    @JvmStatic
+    fun d(obj: Any?) {
+        doLog(ALog::d, obj)
+    }
+
+    @JvmStatic
+    fun i(obj: Any?) {
+        doLog(ALog::i, obj)
+    }
+
+    @JvmStatic
+    fun e(obj: Any?) {
+        doLog(ALog::e, obj, true)
+    }
+
+    @JvmStatic
+    fun v(obj: Any?) {
+        doLog(ALog::v, obj)
+    }
+
+    @JvmStatic
+    fun w(obj: Any?) {
+        doLog(ALog::w, obj)
+    }
+
+    private const val maxLength = 3000
+}
+
