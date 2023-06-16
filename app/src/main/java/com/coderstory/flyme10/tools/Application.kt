9ee450b19a7db1c0b624bc77ffb0a425b9ff1ac6
coderstory <coderstory@163.com>
2023-06-16 14:22:44 +0800

    新增友盟统计sdk

--- app/src/main/java/com/coderstory/flyme10/tools/Application.kt
+++ app/src/main/java/com/coderstory/flyme10/tools/Application.kt
@@ -1,5 +1,19 @@
 package com.coderstory.flyme10.tools
 
 import android.app.Application
+import com.umeng.commonsdk.UMConfigure
 
-class Application : Application()
+class Application : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        UMConfigure.setLogEnabled(true)
+        UMConfigure.preInit(applicationContext, "5ee5d80f978eea081640e210", "weibo")
+        UMConfigure.init(
+            this,
+            "5ee5d80f978eea081640e210",
+            "weibo",
+            UMConfigure.DEVICE_TYPE_PHONE,
+            ""
+        )
+    }
+}
